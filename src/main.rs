83944017d46c8f533abdc98c4b52894a17d3b655
daikihi <daikihi+github@gmail.com>
2025-10-08 19:06:25 +0900

    Load configuration file and map it to struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use crate::libs::config::SpecTrailConfig;
+
+mod libs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let toml_file_name: &str = "src/config/config.toml";
+    let config: SpecTrailConfig = SpecTrailConfig::from_file(toml_file_name)?;
+
+    println!("{:#?}", config);
+    Ok(())
 }
