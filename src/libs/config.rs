4e4035ca06a71b3f7a56961d03c02b7e14fa20f5
daikihi <daikihi+github@gmail.com>
2026-01-15 19:51:29 +0900

    add unit testing on config loading and error handling

--- src/libs/config.rs
+++ src/libs/config.rs
@@ -2,6 +2,9 @@ use serde::Deserialize;
 use std::{fs, io};
 use std::path::Path;
 
+/**
+ * configuration loader from a toml file using with parameter
+ */
 #[derive(Debug, Deserialize)]
 pub struct SpecTrailConfig {
     pub source: SourceConfig,
@@ -30,14 +33,42 @@ impl SpecTrailConfig {
     }
 }
 
+/**
+ * Using for source code specs
+ */
 #[derive(Debug, Deserialize)]
 pub struct SourceConfig {
     pub head: String,
     pub extension: String,
 }
 
+/**
+ * Using for document specs
+ */
 #[derive(Debug, Deserialize)]
 pub struct DocumentConfig {
     pub head: String,
     pub extension: String,
+}
+
+
+
+#[cfg(test)]
+mod tests_spec_trail_config {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let config_result: Result<SpecTrailConfig, _> = SpecTrailConfig::from_file("src/config/config.toml");
+        assert!(config_result.is_ok());
+        let config = config_result.unwrap();
+        println!("{:#?}", config);
+        assert_eq!(config.source.head, ".specify/");
+    }
+
+    #[test]
+    fn it_fails() {
+        let config = SpecTrailConfig::from_file("src/config/config_fail.toml");
+        assert!(config.is_err());
+    }
 }
